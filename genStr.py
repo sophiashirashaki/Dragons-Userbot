d9021aeea2bc2f6a3ffa7f6fe2bbb7b89e8a5528
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-25 19:58:12 +0700

    create genStr.py

---
+++ genStr.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (C) 2021 by TeamDragons, <https://github.com/MIT/TeamDragons>
+#
+# This file is part of <https://github.com/MIT/TeamDragons> project,
+# and is released under the "MIT TeamDragons License Agreement".
+# Please see <https://github.com/MIT/Dragons-Userbot/blob/main/LICENSE>
+#
+# All rights reserved.
+
+exec python3 file/genStrSession.py
