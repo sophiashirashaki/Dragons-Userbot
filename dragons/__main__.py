0ce9538be85570823f82e6f9cf020f6c8eb12e8c
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-26 05:40:19 +0700

    create __main__.py

---
+++ dragons/__main__.py
@@ -0,0 +1,77 @@
+import sys
+
+import dragons
+from dragons import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import catub
+from .utils import (
+    add_bot_to_logger_group,
+    ipchange,
+    load_plugins,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("DragonsUserbot")
+
+print(dragons.__copyright__)
+print("Licensed under the terms of the " + userbot.__license__)
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("Starting Userbot")
+    catub.loop.run_until_complete(setup_bot())
+    LOGS.info("TG Bot Startup Completed")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
+
+
+class CatCheck:
+    def __init__(self):
+        self.sucess = True
+
+
+Catcheck = CatCheck()
+
+
+async def startup_process():
+    check = await ipchange()
+    if check is not None:
+        Catcheck.sucess = False
+        return
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("Yay dragons userbot pengguna Anda secara resmi berfungsi!!!")
+    print(
+        f"Selamat, sekarang ketik {cmdhr}alive untuk melihat pesan jika dragons-userbot aktif\
+        \nJika Anda membutuhkan bantuan, bisa ke https://t.me/KingUserbotSupport"
+    )
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    Catcheck.sucess = True
+    return
+
+
+catub.loop.run_until_complete(startup_process())
+
+if len(sys.argv) not in (1, 3, 4):
+    catub.disconnect()
+elif not Catcheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        catub.run_until_disconnected()
+    except ConnectionError:
+        pass
