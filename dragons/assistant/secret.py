77e5465c6aff45139f6154c6572d1a48339d3027
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-30 04:53:17 +0700

    fixed

--- dragons/assistant/secret.py
+++ dragons/assistant/secret.py
@@ -4,14 +4,14 @@ import re
 
 from telethon.events import CallbackQuery
 
-from userbot import catub
+from dragons import drgub
 
 
-@catub.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
+@drgub.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./userbot/secrets.txt"):
-        jsondata = json.load(open("./userbot/secrets.txt"))
+    if os.path.exists("./dragons/secrets.txt"):
+        jsondata = json.load(open("./dragons/secrets.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
