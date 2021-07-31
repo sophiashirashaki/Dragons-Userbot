371e946bc6d4f4c5ce72215eaf1ab08489fc58bb
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-31 08:36:37 +0700

    fixed

--- dragons/helpers/utils/__init__.py
+++ dragons/helpers/utils/__init__.py
@@ -6,8 +6,8 @@ check = 0
 while flag:
     try:
         from . import format as _format
-        from . import tools as _cattools
-        from . import utils as _catutils
+        from . import tools as _drgtools
+        from . import utils as _drgutils
         from .events import *
         from .format import *
 
