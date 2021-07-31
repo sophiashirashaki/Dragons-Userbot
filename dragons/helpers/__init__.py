dc2821d74aa9f706a5aef495d41b1136f0321663
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-31 08:50:15 +0700

    fixed

--- dragons/helpers/__init__.py
+++ dragons/helpers/__init__.py
@@ -13,7 +13,7 @@ while flag:
         from .progress import *
         from .qhelper import process
         from .tools import *
-        from .utils import _cattools, _catutils, _format
+        from .utils import _drgtools, _drgutils, _format
 
         break
     except ModuleNotFoundError as e:
