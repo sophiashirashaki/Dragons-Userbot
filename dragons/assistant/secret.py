c25d02a5f2e43f2a0037632187b02eebe304520f
Kennedy <abdulmutaqin14@gmail.com>
2021-08-01 12:52:47 +0700

    fixes

--- dragons/assistant/secret.py
+++ dragons/assistant/secret.py
@@ -15,14 +15,14 @@ async def on_plug_in_callback_query_handler(event):
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
-            ids = [userid, catub.uid]
+            ids = [userid, drgub.uid]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
                 reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in catub server"
+            reply_pop_up_alert = "This message no longer exists in drgub server"
     else:
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
