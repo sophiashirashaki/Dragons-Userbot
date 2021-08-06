7a61479e18b5cf90b8b1e1a05580016919d47957
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-07 04:04:48 +0700

    fixed

--- dragons/plugins/antispambot.py
+++ dragons/plugins/antispambot.py
@@ -10,13 +10,13 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from ..Config import Config
 from ..sql_helper.gban_sql_helper import get_gbanuser, is_gbanned
 from ..utils import is_admin
-from . import BOTLOG, BOTLOG_CHATID, catub, edit_or_reply, logging, spamwatch
+from . import BOTLOG, BOTLOG_CHATID, drgub, edit_or_reply, logging, spamwatch
 
 LOGS = logging.getLogger(__name__)
 plugin_category = "admin"
 if Config.ANTISPAMBOT_BAN:
 
-    @catub.on(ChatAction())
+    @drgub.on(ChatAction())
     async def anti_spambot(event):  # sourcery no-metrics
         if not event.user_joined and not event.user_added:
             return
@@ -101,7 +101,7 @@ if Config.ANTISPAMBOT_BAN:
             )
 
 
-@catub.cat_cmd(
+@drgub.cat_cmd(
     pattern="cascheck$",
     command=("cascheck", plugin_category),
     info={
@@ -149,7 +149,7 @@ async def caschecker(event):
     await catevent.edit(text)
 
 
-@catub.cat_cmd(
+@drgub.drg_cmd(
     pattern="spamcheck$",
     command=("spamcheck", plugin_category),
     info={
