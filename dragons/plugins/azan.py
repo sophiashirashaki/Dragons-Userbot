8615202dca0ac763893ddbe32dd1e9972bf393c1
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-09 18:40:19 +0700

    Fix the place

--- dragons/plugins/azan.py
+++ dragons/plugins/azan.py
@@ -1,9 +1,8 @@
 # ported from uniborg
 # https://github.com/muhammedfurkan/UniBorg/blob/master/stdplugins/ezanvakti.py
+
 import json
-
 import requests
-
 from ..sql_helper.globals import gvarstatus
 from . import drgub, edit_delete, edit_or_reply
 
@@ -21,15 +20,12 @@ plugin_category = "extra"
     },
 )
 async def get_adzan(adzan):
-    "Shows you the Islamic prayer times of the given city name"
     input_str = adzan.pattern_match.group(1)
     LOKASI = gvarstatus("DEFCITY") or "Delhi" if not input_str else input_str
     url = f"http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc"
     request = requests.get(url)
     if request.status_code != 200:
-        return await edit_delete(
-            adzan, f"`Couldn't fetch any data about the city {LOKASI}`", 5
-        )
+        return await edit_delete(adzan, f"`Couldn't fetch any data about the city {LOKASI}`", 5)
     result = json.loads(request.text)
     drgresult = f"<b>Islamic prayer times </b>\
             \n\n<b>City     : </b><i>{result['query']}</i>\
