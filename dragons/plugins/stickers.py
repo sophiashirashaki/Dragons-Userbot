e8c443270b2c750cbdc2783135230ff95a14cb83
Kennedy <abdulmutaqin14@gmail.com>
2021-08-11 17:56:44 +0700

    Create stickers.py

---
+++ dragons/plugins/stickers.py
@@ -0,0 +1,388 @@
+import asyncio
+import base64
+import io
+import math
+import random
+import re
+import string
+import urllib.request
+from os import remove
+
+import cloudscraper
+import emoji as drgemoji
+from bs4 import BeautifulSoup as bs
+from PIL import Image
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl.types import (
+    DocumentAttributeFilename,
+    DocumentAttributeSticker,
+    InputStickerSetID,
+    MessageMediaPhoto,
+)
+
+from dragons import drgub
+
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.functions import crop_and_divide
+from ..helpers.tools import media_type
+from ..helpers.utils import _drgtools
+from ..sql_helper.globals import gvarstatus
+
+plugin_category = "fun"
+
+# modified and developed by @mrconfused
+
+
+combot_stickers_url = "https://combot.org/telegram/stickers?q="
+
+EMOJI_SEN = [
+    "Можно отправить несколько смайлов в одном сообщении, однако мы рекомендуем использовать не больше одного или двух на каждый стикер.",
+    "You can list several emoji in one message, but I recommend using no more than two per sticker",
+    "Du kannst auch mehrere Emoji eingeben, ich empfehle dir aber nicht mehr als zwei pro Sticker zu benutzen.",
+    "Você pode listar vários emojis em uma mensagem, mas recomendo não usar mais do que dois por cada sticker.",
+    "Puoi elencare diverse emoji in un singolo messaggio, ma ti consiglio di non usarne più di due per sticker.",
+    "emoji",
+]
+
+KANGING_STR = [
+    "Using Witchery to kang this sticker...",
+    "Plagiarising hehe...",
+    "Inviting this sticker over to my pack...",
+    "Kanging this sticker...",
+    "Hey that's a nice sticker!\nMind if I kang?!..",
+    "hehe me stel ur stikér\nhehe.",
+    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
+    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
+    "Imprisoning this sticker...",
+    "Mr.Steal Your Sticker is stealing this sticker... ",
+]
+
+
+def verify_cond(drgarray, text):
+    return any(i in text for i in drgarray)
+
+
+def pack_name(userid, pack, is_anim):
+    if is_anim:
+        return f"Dragons-Userbot_{userid}_{pack}_anim"
+    return f"Dragons-Userbot_{userid}_{pack}"
+
+
+def char_is_emoji(character):
+    return character in drgemoji.UNICODE_EMOJI["en"]
+
+
+def pack_nick(username, pack, is_anim):
+    if gvarstatus("CUSTOM_STICKER_PACKNAME"):
+        if is_anim:
+            packnick = f"{gvarstatus('CUSTOM_STICKER_PACKNAME')} Vol.{pack} (Animated)"
+        else:
+            packnick = f"{gvarstatus('CUSTOM_STICKER_PACKNAME')} Vol.{pack}"
+    elif is_anim:
+        packnick = f"@{username} Vol.{pack} (Animated)"
+    else:
+        packnick = f"@{username} Vol.{pack}"
+    return packnick
+
+
+async def resize_photo(photo):
+    """Resize the given photo to 512x512"""
+    image = Image.open(photo)
+    if (image.width and image.height) < 512:
+        size1 = image.width
+        size2 = image.height
+        if image.width > image.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        image = image.resize(sizenew)
+    else:
+        maxsize = (512, 512)
+        image.thumbnail(maxsize)
+    return image
+
+
+async def newpacksticker(
+    drgevent,
+    conv,
+    cmd,
+    args,
+    pack,
+    packnick,
+    stfile,
+    emoji,
+    packname,
+    is_anim,
+    otherpack=False,
+    pkang=False,
+):
+    await conv.send_message(cmd)
+    await conv.get_response()
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.send_message(packnick)
+    await conv.get_response()
+    await args.client.send_read_acknowledge(conv.chat_id)
+    if is_anim:
+        await conv.send_file("AnimatedSticker.tgs")
+        remove("AnimatedSticker.tgs")
+    else:
+        stfile.seek(0)
+        await conv.send_file(stfile, force_document=True)
+    rsp = await conv.get_response()
+    if not verify_cond(EMOJI_SEN, rsp.text):
+        await drgevent.edit(
+            f"Failed to add sticker, use @Stickers bot to add the sticker manually.\n**error :**{rsp}"
+        )
+        return
+    await conv.send_message(emoji)
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.get_response()
+    await conv.send_message("/publish")
+    if is_anim:
+        await conv.get_response()
+        await conv.send_message(f"<{packnick}>")
+    await conv.get_response()
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.send_message("/skip")
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.get_response()
+    await conv.send_message(packname)
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.get_response()
+    await args.client.send_read_acknowledge(conv.chat_id)
+    if not pkang:
+        return otherpack, packname, emoji
+    return pack, packname
+
+
+async def add_to_pack(
+    drgevent,
+    conv,
+    args,
+    packname,
+    pack,
+    userid,
+    username,
+    is_anim,
+    stfile,
+    emoji,
+    cmd,
+    pkang=False,
+):
+    await conv.send_message("/addsticker")
+    await conv.get_response()
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.send_message(packname)
+    x = await conv.get_response()
+    while ("50" in x.text) or ("120" in x.text):
+        try:
+            val = int(pack)
+            pack = val + 1
+        except ValueError:
+            pack = 1
+        packname = pack_name(userid, pack, is_anim)
+        packnick = pack_nick(username, pack, is_anim)
+        await drgevent.edit(
+            f"`Switching to Pack {str(pack)} due to insufficient space`"
+        )
+        await conv.send_message(packname)
+        x = await conv.get_response()
+        if x.text == "Invalid pack selected.":
+            return await newpacksticker(
+                drgevent,
+                conv,
+                cmd,
+                args,
+                pack,
+                packnick,
+                stfile,
+                emoji,
+                packname,
+                is_anim,
+                otherpack=True,
+                pkang=pkang,
+            )
+    if is_anim:
+        await conv.send_file("AnimatedSticker.tgs")
+        remove("AnimatedSticker.tgs")
+    else:
+        stfile.seek(0)
+        await conv.send_file(stfile, force_document=True)
+    rsp = await conv.get_response()
+    if not verify_cond(EMOJI_SEN, rsp.text):
+        await drgevent.edit(
+            f"Failed to add sticker, use @Stickers bot to add the sticker manually.\n**error :**{rsp}"
+        )
+        return
+    await conv.send_message(emoji)
+    await args.client.send_read_acknowledge(conv.chat_id)
+    await conv.get_response()
+    await conv.send_message("/done")
+    await conv.get_response()
+    await args.client.send_read_acknowledge(conv.chat_id)
+    if not pkang:
+        return packname, emoji
+    return pack, packname
+
+
+@drgub.drg_cmd(
+    pattern="kang(?:\s|$)([\s\S]*)",
+    command=("kang", plugin_category),
+    info={
+        "header": "To kang a sticker.",
+        "description": "Kang's the sticker/image to the specified pack and uses the emoji('s) you picked",
+        "usage": "{tr}kang [emoji('s)] [number]",
+    },
+)
+async def kang(args):  # sourcery no-metrics
+    "To kang a sticker."
+    photo = None
+    emojibypass = False
+    is_anim = False
+    emoji = None
+    message = await args.get_reply_message()
+    user = await args.client.get_me()
+    if not user.username:
+        try:
+            user.first_name.encode("utf-8").decode("ascii")
+            username = user.first_name
+        except UnicodeDecodeError:
+            username = f"drg_{user.id}"
+    else:
+        username = user.username
+    userid = user.id
+    if message and message.media:
+        if isinstance(message.media, MessageMediaPhoto):
+            drgevent = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
+            photo = io.BytesIO()
+            photo = await args.client.download_media(message.photo, photo)
+        elif "image" in message.media.document.mime_type.split("/"):
+            drgevent = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
+            photo = io.BytesIO()
+            await args.client.download_file(message.media.document, photo)
+            if (
+                DocumentAttributeFilename(file_name="sticker.webp")
+                in message.media.document.attributes
+            ):
+                emoji = message.media.document.attributes[1].alt
+                emojibypass = True
+        elif "tgsticker" in message.media.document.mime_type:
+            drgevent = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
+            await args.client.download_file(
+                message.media.document, "AnimatedSticker.tgs"
+            )
+
+            attributes = message.media.document.attributes
+            for attribute in attributes:
+                if isinstance(attribute, DocumentAttributeSticker):
+                    emoji = attribute.alt
+            emojibypass = True
+            is_anim = True
+            photo = 1
+        else:
+            await edit_delete(args, "`Unsupported File!`")
+            return
+    else:
+        await edit_delete(args, "`I can't kang that...`")
+        return
+    if photo:
+        splat = ("".join(args.text.split(maxsplit=1)[1:])).split()
+        emoji = emoji if emojibypass else "😂"
+        pack = 1
+        if len(splat) == 2:
+            if char_is_emoji(splat[0][0]):
+                if char_is_emoji(splat[1][0]):
+                    return await drgevent.edit("check `.info stickers`")
+                pack = splat[1]  # User sent both
+                emoji = splat[0]
+            elif char_is_emoji(splat[1][0]):
+                pack = splat[0]  # User sent both
+                emoji = splat[1]
+            else:
+                return await drgevent.edit("check `.info stickers`")
+        elif len(splat) == 1:
+            if char_is_emoji(splat[0][0]):
+                emoji = splat[0]
+            else:
+                pack = splat[0]
+        packnick = pack_nick(username, pack, is_anim)
+        packname = pack_name(userid, pack, is_anim)
+        cmd = "/newpack"
+        stfile = io.BytesIO()
+        if is_anim:
+            cmd = "/newanimated"
+        else:
+            image = await resize_photo(photo)
+            stfile.name = "sticker.png"
+            image.save(stfile, "PNG")
+        response = urllib.request.urlopen(
+            urllib.request.Request(f"http://t.me/addstickers/{packname}")
+        )
+        htmlstr = response.read().decode("utf8").split("\n")
+        if (
+            "  A <strong>Telegram</strong> user has created the <strong>Sticker&nbsp;Set</strong>."
+            not in htmlstr
+        ):
+            async with args.client.conversation("Stickers") as conv:
+                packname, emoji = await add_to_pack(
+                    drgevent,
+                    conv,
+                    args,
+                    packname,
+                    pack,
+                    userid,
+                    username,
+                    is_anim,
+                    stfile,
+                    emoji,
+                    cmd,
+                )
+            await edit_delete(
+                drgevent,
+                f"`Sticker kanged successfully!\
+                    \nYour Pack is` [here](t.me/addstickers/{packname}) `and emoji for the kanged sticker is {emoji}`",
+                parse_mode="md",
+                time=10,
+            )
+        else:
+            await drgevent.edit("`Brewing a new Pack...`")
+            async with args.client.conversation("Stickers") as conv:
+                otherpack, packname, emoji = await newpacksticker(
+                    drgevent,
+                    conv,
+                    cmd,
+                    args,
+                    pack,
+                    packnick,
+                    stfile,
+                    emoji,
+                    packname,
+                    is_anim,
+                )
+            if otherpack:
+                await edit_delete(
+                    drgevent,
+                    f"`Sticker kanged to a Different Pack !\
+                    \nAnd Newly created pack is` [here](t.me/addstickers/{packname}) `and emoji for the kanged sticker is {emoji}`",
+                    parse_mode="md",
+                    time=10,
+                )
+            else:
+                await edit_delete(
+                    drgevent,
+                    f"`Sticker kanged successfully!\
+                    \nYour Pack is` [here](t.me/addstickers/{packname}) `and emoji for the kanged sticker is {emoji}`",
+                    parse_mode="md",
+                    time=10,
+                )
