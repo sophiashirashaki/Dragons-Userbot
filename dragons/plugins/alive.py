b058a12524b8b2fdeaba5feab0c95d3b032e7333
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-13 15:45:55 +0700

    update: auto fixes
    • translate help the en to id 
    • updated alive userbot
    • help auto fixes
    • code auto fixes 
    • alive auto fixes code
    • recoding
    
    Author by : apisuserbot
    Developer : TeamDragons 
    Repository : Dragons-Userbot

--- dragons/plugins/alive.py
+++ dragons/plugins/alive.py
@@ -1,7 +1,6 @@
 import random
 import re
 import time
-import asyncio
 
 from platform import python_version
 from telethon import version
@@ -27,51 +26,56 @@ plugin_category = "utils"
     pattern="alive$",
     command=("alive", plugin_category),
     info={
-        "header": "To check bot's alive status",
-        "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
+        "header": "Untuk memeriksa status hidup bot",
+        "options": "Untuk menampilkan media dalam perintah ini, Anda perlu `set ALIVE_PIC` dengan tautan media, dapatkan ini dengan membalas media dengan `{tr}tgm`",
         "usage": [
             "{tr}alive",
         ],
     },
 )
 async def amireallyalive(event):
-    "A kind of showing bot details"
+    "Semacam menampilkan detail bot"
     reply_to_id = await reply_id(event)
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
+    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "✮ DRAGONS-USERBOT ✮"
     DRG_IMG = gvarstatus("ALIVE_PIC")
     if DRG_IMG:
         DRG = [x for x in DRG_IMG.split()]
         A_IMG = list(DRG)
         PIC = random.choice(A_IMG)
-        drg_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
+        drg_caption = f"=============================\n"
+        drg_caption += f"__**{CUSTOM_ALIVE_TEXT}**__\n"
+        drg_caption += f"============================\n"
         drg_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
         drg_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
         drg_caption += f"**{EMOJI} Dragons-userbot Version :** `{drgversion}`\n"
         drg_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
         drg_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
-        drg_caption += f"**{EMOJI} Master:** {mention}\n"
+        drg_caption += f"**{EMOJI} Master :** {mention}\n"
+        drg_caption += f"============================\n"
         try:
             await event.client.send_file(event.chat_id, PIC, caption=drg_caption, reply_to=reply_to_id)
-            await asyncio.sleep(1000)
             await event.delete()
         except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
             return await edit_or_reply(
                 event,
-                f"**Media Value Error!!**\n__Change the link by __`.setdv`\n\n**__Can't get media from this link :-**__ `{PIC}`",
+                f"**Media Value Error!!**\n__Ubah tautan dengan__ `.setdv`\n\n**__Tidak bisa mendapatkan media dari tautan ini :-**__ `{PIC}`",
             )
     else:
         await edit_or_reply(
             event,
-            f"**{CUSTOM_ALIVE_TEXT}**\n\n"
+            f"=============================\n"
+            f"__**{CUSTOM_ALIVE_TEXT}**__\n\n"
+            f"=============================\n"
             f"**{EMOJI} Database :** `{check_sgnirts}`\n"
             f"**{EMOJI} Telethon Version :** `{version.__version__}\n`"
             f"**{EMOJI} Dragons-userbot Version :** `{drgversion}`\n"
             f"**{EMOJI} Python Version :** `{python_version()}\n`"
             f"**{EMOJI} Uptime :** `{uptime}\n`"
-            f"**{EMOJI} Master:** {mention}\n",
+            f"**{EMOJI} Master:** {mention}\n"
+            f"=============================\n",
         )
 
 
@@ -79,25 +83,24 @@ async def amireallyalive(event):
     pattern="ialive$",
     command=("ialive", plugin_category),
     info={
-        "header": "To check bot's alive status via inline mode",
-        "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
+        "header": "Untuk memeriksa status hidup bot melalui mode inline",
+        "options": "Untuk menampilkan media dalam hal ini perintah kamu butuh `set ALIVE_PIC` dengan tautan media, dapatkan ini dengan membalas media dengan `{tr}tgm`",
         "usage": [
             "{tr}alive",
         ],
     },
 )
 async def amireallyalive(event):
-    "A kind of showing bot details by your inline bot"
+    "Semacam menampilkan detail bot oleh bot inline Anda"
     reply_to_id = await reply_id(event)
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    drg_caption = f"**Dragons-userbot is Up and Running**\n"
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "✥"
+    drg_caption = f"**Dragons-userbot sedang berjalan**\n"
     drg_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
     drg_caption += f"**{EMOJI} Dragons-userbot Version :** `{drgversion}`\n"
     drg_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-    drg_caption += f"**{EMOJI} Master:** {mention}\n"
+    drg_caption += f"**{EMOJI} Master :** {mention}\n"
     results = await event.client.inline_query(Config.TG_BOT_USERNAME, drg_caption)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
-    await asyncio.sleep(1000)
     await event.delete()
 
 
